bb68313bd74e6a62e5eba8a5318614e95515ed4c
SANTOS <mbsantos@fit.edu.ph>
2025-10-22 15:01:04 +0800

    Changing the line to square

--- Comvis Final Project/Comvis Final Project.cpp
+++ Comvis Final Project/Comvis Final Project.cpp
@@ -6,9 +6,11 @@ using namespace std;
 void display() { //placeholder
 	glClear(GL_COLOR_BUFFER_BIT); 
 	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_LINES);
-	glVertex2f(-0.5, 0.5);
+	glBegin(GL_POLYGON);
 	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, 0.5);
 	glEnd();
 	glFlush(); 
 }
