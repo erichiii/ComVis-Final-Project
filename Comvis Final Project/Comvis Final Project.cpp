c86d528ce3c83c3f100841fc755982f12cc45fef
ddavidd <kristiandavidbautista@gmail.com>
2025-10-22 14:56:38 +0800

    changed background color to sky blue

--- Comvis Final Project/Comvis Final Project.cpp
+++ Comvis Final Project/Comvis Final Project.cpp
@@ -4,19 +4,20 @@
 using namespace std;
 
 void display() { //placeholder
-	glClear(GL_COLOR_BUFFER_BIT); 
+	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
 	glBegin(GL_LINES);
 	glVertex2f(-0.5, 0.5);
 	glVertex2f(0.5, 0.5);
 	glEnd();
-	glFlush(); 
+	glFlush();
 }
 int main(int argc, char** argv) {
-	glutInit(&argc, argv); 
+	glutInit(&argc, argv);
 	glutInitWindowSize(1920, 1080);
-	glutCreateWindow("Playground Scene"); 
-	glutDisplayFunc(display); 
-	glutMainLoop(); 
+	glutCreateWindow("Playground Scene");
+	glClearColor(0.529f, 0.808f, 0.922f, 1.0f);
+	glutDisplayFunc(display);
+	glutMainLoop();
 	return 0;
 }
